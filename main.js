b115cf1624fe1ef91cc33f9f5c62acdbd9bad5d5
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-08 12:14:20 -0500

    added french js functionality

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
-let translator = {
-    spanish: [
-        {english: "Season", translation: "Temporada"},
-        {english: "Greetings", translation: "Saludos"},
-        {english: "and", translation: "y"},
-        {english: "Happy", translation: "Contento"},
-        {english: "Holidays", translation: "Vacaciones"},
-    ],
-    french: [
-        {english: "Season", translation: "Saison"},
-        {english: "Greetings", translation: "Salutations"},
-        {english: "and", translation: "et"},
-        {english: "Happy", translation: "Content"},
-        {english: "Holidays", translation: "Vacances"},
-    ]
-
-}
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+
+const spanish =
+        {"Season": "Temporada",
+        "Greetings": "Saludos", 
+        "and": "y",
+        "Happy": "Contento", 
+        "Holidays": "Vacaciones"
+        };
+ 
+const french =
+        {"Season": "Saison",
+        "Greetings": "Salutations", 
+        "and": "et",
+        "Happy": "Content", 
+        "Holidays": "Vacances"
+        };
+
+const italian =
+        {"Season": "Stagione",
+        "Greetings": "Saluti", 
+        "and": "e",
+        "Happy": "Contento", 
+        "Holidays": "Vacanze"
+        };
+
+/// Spanish Button ///        
+
+const translatedSpanishText = () => {
+   let inputText = document.getElementById('textArea').value;
+   let inputWords = inputText.split(' ');
+   for (i = 0; i < inputWords.length; i++) {
+      console.log(spanish[inputWords[i]]); 
+   }
+};
+
+const spanishButton = document.getElementById('Spanish');
+spanishButton.addEventListener("click", translatedSpanishText);
+
+/// French Button ///
+
+const translatedFrenchText = () => {
+    let inputText = document.getElementById('textArea').value;
+    let inputWords = inputText.split(' ');
+    for (i = 0; i < inputWords.length; i++) {
+       console.log(french[inputWords[i]]); 
+    }
+ };
+ 
+ const frenchButton = document.getElementById('French');
+ frenchButton.addEventListener("click", translatedFrenchText);
+
+ /// Italian Button ///
 
