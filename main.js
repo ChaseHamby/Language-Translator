586d0b8e67915a934a09d441cdaed25065a427d9
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-08 12:04:07 -0500

    coded js for spanish button functionality

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
-let translator = {
-    spanish: [
-        {english: "Season", translation: "Temporada"},
-        {english: "Greetings", translation: "Saludos"},
-        {english: "and", translation: "y"},
-        {english: "Happy", translation: "Contento"},
-        {english: "Holidays", translation: "Vacaciones"},
-    ]
-
-
-
-
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+
+const spanish =
+        {"Season": "Temporada",
+        "Greetings": "Saludos", 
+        "and": "y",
+        "Happy": "Contento", 
+        "Holidays": "Vacaciones"
+        };
+
+    
+const translatedSpanishText = () => {
+   let inputText = document.getElementById('textArea').value;
+   let inputWords = inputText.split(' ');
+   for (i = 0; i < inputWords.length; i++) {
+      console.log(spanish[inputWords[i]]);
+       
+   }
+};
+
+const spanishButton = document.getElementById('Spanish');
+spanishButton.addEventListener("click", translatedSpanishText);
+
 
