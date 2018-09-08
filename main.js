505b62af8e091c5544688d2cb79863652a033e21
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-08 12:28:55 -0500

    added js functionality for italian button

--- main.js
+++ main.js
@@ -1,29 +1,73 @@
-let translator = {
-    spanish: [
-        {english: "Season", translation: "Temporada"},
-        {english: "Greetings", translation: "Saludos"},
-        {english: "and", translation: "y"},
-        {english: "Happy", translation: "Contento"},
-        {english: "Holidays", translation: "Vacaciones"},
-    ],
-    french: [
-        {english: "Season", translation: "Saison"},
-        {english: "Greetings", translation: "Salutations"},
-        {english: "and", translation: "et"},
-        {english: "Happy", translation: "Content"},
-        {english: "Holidays", translation: "Vacances"},
-    ],
-    italian: [
-        {english: "Season", translation: "Stagione"},
-        {english: "Greetings", translation: "Saluti"},
-        {english: "and", translation: "e"},
-        {english: "Happy", translation: "Contento"},
-        {english: "Holidays", translation: "Vacanze"},
-    ],
-}
 
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+
+const spanish =
+        {"Season": "Temporada",
+        "Greetings": "Saludos", 
+        "and": "y",
+        "Happy": "Contento", 
+        "Holidays": "Vacaciones"
+        };
+ 
+const french =
+        {"Season": "Saison",
+        "Greetings": "Salutations", 
+        "and": "et",
+        "Happy": "Content", 
+        "Holidays": "Vacances"
+        };
+
+const italian =
+        {"Season": "Stagione",
+        "Greetings": "Saluti", 
+        "and": "e",
+        "Happy": "Contento", 
+        "Holidays": "Vacanze"
+        };
+
+/// Spanish Button ///        
+
+let output = '';
+const translatedSpanishText = () => {
+   let inputText = document.getElementById('textArea').value;
+   let inputWords = inputText.split(' ');
+   for (i = 0; i < inputWords.length; i++) {
+    output = (spanish[inputWords[i]]); 
+   }
+   printToDom(output,'emptyDiv');
+};
+
+const spanishButton = document.getElementById('Spanish');
+spanishButton.addEventListener("click", translatedSpanishText);
+
+/// French Button ///
+
+const translatedFrenchText = () => {
+    let inputText = document.getElementById('textArea').value;
+    let inputWords = inputText.split(' ');
+    for (i = 0; i < inputWords.length; i++) {
+       output = (french[inputWords[i]]); 
+    }
+    printToDom(output,'emptyDiv');
+ };
+ 
+ const frenchButton = document.getElementById('French');
+ frenchButton.addEventListener("click", translatedFrenchText);
+
+ /// Italian Button ///
+
+ const translatedItalianText = () => {
+    let inputText = document.getElementById('textArea').value;
+    let inputWords = inputText.split(' ');
+    for (i = 0; i < inputWords.length; i++) {
+       output= (italian[inputWords[i]]); 
+    }
+    printToDom(output,'emptyDiv');
+ };
+ 
+ const italianButton = document.getElementById('Italian');
+ italianButton.addEventListener("click", translatedItalianText);
 
