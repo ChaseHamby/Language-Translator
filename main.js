d86390ccd0dba46502de97be09de0bddd550e20b
Chase Hamby <c.chase.hamby@gmail.com>
2018-09-08 09:06:35 -0500

    added spanish button and info

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+let translator = {
+    spanish: [
+        {english: "Season", translation: "Temporada"},
+        {english: "Greetings", translation: "Saludos"},
+        {english: "and", translation: "y"},
+        {english: "Happy", translation: "Contento"},
+        {english: "Holidays", translation: "Vacaciones"},
+    ]
+
+
+
+
+
 const printToDom = (stringToPrint, divId) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML += stringToPrint;
 };
+
